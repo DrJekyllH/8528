7ba1a096e804134a01bcee9555a45d544e951fd7
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-07-27 10:23:24 +0900

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,8 @@
-const headerNavLink = document.querySelectorAll('.js-header-nav-link');
-headerNavLink.forEach((targetLink) => {
-    if (targetLink.href === location.href) {
-  targetLink.parentElement.classList.add('is-current');
-  }
+window.addEventListener('DOMContentLoaded', function(){
+	const headerNavLink = document.querySelectorAll('.js-header-nav-link');
+	headerNavLink.forEach((targetLink) => {
+		if (targetLink.href === location.href) {
+	  targetLink.parentElement.classList.add('is-current');
+	  }
+	});
 });
