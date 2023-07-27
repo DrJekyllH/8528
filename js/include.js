b41c370a8cbe7bf0fc95ae5cb710e086957d0e3c
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-07-27 12:44:14 +0900

    Add files via upload

--- js/include.js
+++ js/include.js
@@ -5,6 +5,12 @@ includeHeader.onreadystatechange = function () {
     const headerHTML = includeHeader.responseText;
     const header = document.querySelector("#header");
     header.insertAdjacentHTML("afterbegin", headerHTML);
+	const headerNavLink = document.querySelectorAll('.js-header-nav-link');
+	headerNavLink.forEach((targetLink) => {
+		if (targetLink.href === location.href) {
+	  targetLink.parentElement.classList.add('is-current');
+	  }
+	});
   }
 };
 includeHeader.send();
