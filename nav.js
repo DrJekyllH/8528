7fd57c6321694ff6adce36925bda78c968163479
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-07-27 09:04:11 +0900

    Delete nav.js

--- nav.js
+++
@@ -1,7 +0,0 @@
-const headerNavLink = document.querySelectorAll('.nav-link');
-
-headerNavLink.forEach((targetLink) => {
-  if (targetLink.href === location.href) {
-    targetLink.parentElement.classList.add('is-current');
-  }
-});
