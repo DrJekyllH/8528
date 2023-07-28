f5a70f227602f32a61052c103a1b5591fb4e7ba2
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-07-28 13:52:22 +0900

    Update youtube.js

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,11 @@
 onload = async () => {
-    const res = await fetch("https://www.youtube.com/@nomuranimu2nd");
-console.log(res);
+//    const res = await fetch("https://www.youtube.com/@nomuranimu2nd");
+//console.log(res);
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://api.8528.space/youtube.php', true);
+    request.onload = function () {
+      var data = this.response;
+      console.log(data);
+    }
+    request.send();
 }
