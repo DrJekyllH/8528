c5e723b5f1011c33af063b76f216896452ae18bb
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-07-31 23:10:10 +0900

    Add files via upload

--- js/discordapi.js
+++ js/discordapi.js
@@ -1,10 +1,10 @@
 
 function discord_widget(){
 var request = new XMLHttpRequest();
-    request.open('GET', 'https://api.8528.space/discord.php', true);
+    request.open('GET', 'https://api.8528.space/discord2.php', true);
     request.onload = function() {
         var data = JSON.parse(this.response);
-        innerhtml = `<div><i class="fa-brands fa-discord" style="color: #7289da;"></i> ` + data.online + `</div>`;
+        innerhtml = `<div><i class="fa-brands fa-discord" style="color: #7289da;"></i> ` + data.online + `/` + data.members + `</div>`;
         widget = document.getElementById('discord');
         widget.innerHTML = innerhtml;
         setTimeout(discord_widget, 60000);
