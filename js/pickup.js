c59d3cd8d231824c8b7603dc58498c14ffee968a
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-07-31 23:39:28 +0900

    Add files via upload

--- js/pickup.js
+++ js/pickup.js
@@ -38,7 +38,8 @@ let innerHTML = `
 `;
 }else{
 innerHTML=`
-<div style="width: 384px; height: 216px; border: 1px solid; display:flex; align-items:center;"><h1>🐥</h1></div><div class="video-title">嵐の前の静けさだっぴ・・・</div>
+<div style="width: 384px; height: 216px; border: 1px solid; display:flex; align-items:center;">
+<h1 style="filter:drop-shadow(5px -1px 2px #808080);">🐥</h1></div><div class="video-title">嵐の前の静けさだっぴ・・・</div>
 `;
 }
 
