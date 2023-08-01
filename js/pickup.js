0b84e48b727732fe71e9f392ca14b8ed7692d141
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-08-01 23:53:52 +0900

    Add files via upload

--- js/pickup.js
+++ js/pickup.js
@@ -5,6 +5,7 @@ var cont_class = "video-container";
 const request = fetch('https://api.8528.space/stream.php?p=1')
 .then(response=> response.json())
 .then(data=>{
+let html;
 if(data.length > 0){
 switch (data[0].status) {
 	case "live":
@@ -30,14 +31,14 @@ var title_alt = data[0].title;
 if (data[0].title.length > 48) data[0].title = data[0].title.substr(0, 46) + '…';
 
 
-let innerHTML = `
+html = `
 <a href="` + urlformat + data[0].vid + `" class="video-link" target="_blank" rel="noopener" title="` + title_alt + `">
 <img class="pickup-video-thumnail" src="` + data[0].turl + `"><br>
 <div class="video-title">` + data[0].title + `</div></a>
 <div class="video-channel"><a href="` + data[0].curl + `" target="_blank" rel="noopener noreferrer">` + plf_icon + ` ` + data[0].name + `</a><span style="margin-left:10rem;">更新: ` + data[0].time + `</span></div>
 `;
 }else{
-innerHTML=`
+html =`
 <div style="width: 384px; height: 216px; border: 1px solid; display:flex; align-items:center; flex-direction: column; overflow:hidden;">
 <h1 class="buruburu" style="filter:drop-shadow(5px -1px 2px #808080);">🐥</h1>
 <span style="color: transparent;text-shadow: 0 0 10px rgba(0,0,0,1); font-size: 200rem;">𝕏</span>
@@ -47,7 +48,7 @@ innerHTML=`
 }
 
 let player = document.getElementById('player');
-player.innerHTML = innerHTML;
+player.innerHTML = html;
 
 });
 
