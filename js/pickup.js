0a96a7edc04b9a6614578b15cca415413a814d5f
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-08-02 16:47:05 +0900

    Add files via upload

--- js/pickup.js
+++ js/pickup.js
@@ -41,9 +41,9 @@ html = `
 html =`
 <div style="width: 384px; height: 216px; border: 1px solid; display:flex; align-items:center; flex-direction: column; overflow:hidden;">
 <h1 class="buruburu" style="filter:drop-shadow(5px -1px 2px #808080);">
-<img src="images/front-facing_baby_chick_flat.svg" style="vertical-align: text-bottom; width: 28px;">
+<img src="images/front-facing_baby_chick_flat.svg" style="vertical-align: text-bottom; width: 28px; fill-opacity: 0.6 !important;">
 </h1>
-<span style="color: transparent;text-shadow: 0 0 10px rgba(0,0,0,1); font-size: 200rem;">𝕏</span>
+<img src="images/X.svg" style="filter: drop-shadow(0 -60px 10px rgba(0,0,0,1)); width: 200px; padding: 150px;">
 </div>
 <div class="video-title">嵐の前の静けさだっぴ・・・</div>
 `;
