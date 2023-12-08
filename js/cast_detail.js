3b8fa3f4041d7358a8d21e4212f8536359c1b4ad
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-12-08 17:13:26 +0900

    Add files via upload

--- js/cast_detail.js
+++ js/cast_detail.js
@@ -41,69 +41,68 @@ window.onload = function () {
         openFile("https://api.8528.space/cast.php?id=" + searchParams.get("id"))
             .then((response) => {
                 let data = JSON.parse(response);
-                //console.log(data);
-                if (data[1]) {
+                console.log(data);
+                if (data["名前"]) {
                     var namae_div = document.createElement('div');
                     namae_div.className = 'name_jp';
-                    namae_div.innerHTML = data[1];
+                    namae_div.innerHTML = data["名前"];
                     name.appendChild(namae_div);
-
                 }
-                if (data[2]) {
+                if (data["Name"]) {
                     let namae_div = document.createElement('div');
                     namae_div.className = 'name_en';
-                    namae_div.innerHTML = data[2];
+                    namae_div.innerHTML = data["Name"];
                     name.appendChild(namae_div);
                 }
-                if (data[8]) {
+                if (data["誕生日"]) {
                     //誕生日
                     let birthday_span = document.createElement('div');
                     birthday_span.className = 'birthday';
-                    birthday_span.innerHTML = '誕生日：' + data[8];
+                    birthday_span.innerHTML = '誕生日：' + data["誕生日"];
                     profile.appendChild(birthday_span);
 
                 }
-                if (data[9]) {
+                if (data["身長"]) {
                     //身長
                     let height_span = document.createElement('div');
                     height_span.className = 'height';
-                    height_span.innerHTML = '身長：' + data[9];
+                    height_span.innerHTML = '身長：' + data["身長"];
                     profile.appendChild(height_span);
 
                 }
-                if (data[7]) {
+                if (data["テーマ色（コード）"]) {
                     //キャラ色
                     let color_span = document.createElement('div');
                     color_span.className = 'color';
-                    color_span.innerHTML = 'カラー：' + data[7] + ' <span class="color-sample" style="background-color:' + data[7] + ';">&nbsp;</span>';
+                    color_span.innerHTML = 'カラー：' + data["テーマ色（コード）"] + ' <span class="color-sample" style="background-color:' + data["テーマ色（コード）"] + ';">&nbsp;</span>';
                     profile.appendChild(color_span);
                     let container = document.getElementById("container");
-                    container.style.background = "linear-gradient(200deg, " + data[7] + ", #ffffff)";
+                    container.style.background = "linear-gradient(200deg, " + data["テーマ色（コード）"] + ", #ffffff)";
 
                 }
-                if (data[6]) {
+                if (data["推しマーク"]) {
                     //推しマーク
                     let mark_span = document.createElement('div');
                     mark_span.className = 'mark';
-                    mark_span.innerHTML = 'マーク：' + data[6];
+                    mark_span.innerHTML = 'マーク：' + data["推しマーク"];
                     profile.appendChild(mark_span);
 
                 }
 
-                if (data[10]) {
+                if (data["ファンネーム"]) {
                     //ファンネーム
                     let fan_span = document.createElement('div');
                     fan_span.className = 'fan';
-                    fan_span.innerHTML = 'ファンネーム：' + data[10];
+                    fan_span.innerHTML = 'ファンネーム：' + data["ファンネーム"];
                     profile.appendChild(fan_span);
 
                 }
 
-                if (data[11]) {
+                if (data["おすすめ動画（Youtube）"]) {
                     //おすすめ動画
-                    data[11] = JSON.stringify(data[11]).replace(/"/g, '');
+                    data["おすすめ動画（Youtube）"] = JSON.stringify(data["おすすめ動画（Youtube）"]).replace(/"/g, '');
                     //console.log(data[11]);
-                    const vid = new URL(data[11]).searchParams;
+                    const vid = new URL(data["おすすめ動画（Youtube）"]).searchParams;
                     //console.log(vid.get("v"));
                     if (vid.get("v")) {
                         let video_span = document.createElement('div');
@@ -114,50 +113,50 @@ window.onload = function () {
 
                 }
 
-                if (data[12]) {
+                if (data["YouTube"]) {
                     //Youtube
                     let link_span = document.createElement('span');
                     link_span.className = 'links';
-                    link_span.innerHTML = '<a href="' + data[12] + '" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-youtube fa-2xl" style="color: #ff0000;"></i> Youtube</a>';
+                    link_span.innerHTML = '<a href="' + data["YouTube"] + '" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-youtube fa-2xl" style="color: #ff0000;"></i> Youtube</a>';
                     link.appendChild(link_span);
                 }
-                if (data[13]) {
+                if (data["Twitch"]) {
                     //Twitch
                     let link_span = document.createElement('span');
                     link_span.className = 'links';
-                    link_span.innerHTML = '<a href="' + data[13] + '" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-twitch fa-2xl" style="color: #6441a5;""></i> Twitch</a>';
+                    link_span.innerHTML = '<a href="' + data["Twitch"] + '" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-twitch fa-2xl" style="color: #6441a5;""></i> Twitch</a>';
                     link.appendChild(link_span);
                 }
-                if (data[14]) {
+                if (data["X"]) {
                     //X
                     let link_span = document.createElement('span');
                     link_span.className = 'links';
-                    link_span.innerHTML = '<a href="' + data[14] + '" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-square-x-twitter fa-2xl" style="color: #000000;"></i> X（旧Twitter）</a>';
+                    link_span.innerHTML = '<a href="' + data["X"] + '" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-square-x-twitter fa-2xl" style="color: #000000;"></i> X（旧Twitter）</a>';
                     link.appendChild(link_span);
                 }
-                if (data[15]) {
+                if (data["ショップ"]) {
                     //ショップ
                     let link_span = document.createElement('span');
                     link_span.className = 'links';
-                    link_span.innerHTML = '<a href="' + data[15] + '" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-basket-shopping fa-2xl" style="color: #000000;"></i> ショップ</a>';
+                    link_span.innerHTML = '<a href="' + data["ショップ"] + '" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-basket-shopping fa-2xl" style="color: #000000;"></i> ショップ</a>';
                     link.appendChild(link_span);
                 }
-                if (data[5]) {
+                if (data["紹介文"]) {
                     //紹介文
-                    data[5] = JSON.stringify(data[5]).replace(/\\r\\n|\\r|\\n/g, '<br>');
-                    data[5] = JSON.parse(data[5]);
+                    data["紹介文"] = JSON.stringify(data["紹介文"]).replace(/\\r\\n|\\r|\\n/g, '<br>');
+                    data["紹介文"] = JSON.parse(data["紹介文"]);
                     let intro_div = document.createElement('div');
                     intro_div.className = 'intro';
-                    intro_div.innerHTML = data[5];
+                    intro_div.innerHTML = data["紹介文"];
                     intro.appendChild(intro_div);
                 }
-                if (data[4]) {
+                if (data["立ち絵（背景透過）"]) {
                     var img = new Image();
                     img.onload = function () {
                         const loader = document.getElementById('loader');
                         loader.classList.add('loaded');
                     };
-                    img.src = data[4];
+                    img.src = data["立ち絵（背景透過）"];
                     img.style.width = "100%";
                     img.style.alignSelf = "flex-end";
                     left.appendChild(img);
