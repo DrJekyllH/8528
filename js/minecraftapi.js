a2d9a46e936bd4a4e26955547ef517825351c90c
DrJekyllH <140363967+DrJekyllH@users.noreply.github.com>
2023-12-17 00:02:20 +0900

    Update minecraftapi.js

--- js/minecraftapi.js
+++ js/minecraftapi.js
@@ -3,7 +3,7 @@ var request = new XMLHttpRequest();
     request.open('GET', 'https://api.8528.space/minecraft.php', true);
     request.onload = function() {
         var data = JSON.parse(this.response);
-        innerhtml = `<div><i class="fa-solid fa-cube" style="color: #57b545;"></i> ` + data.online + `/` + data.max + `</div>`;
+        innerhtml = `<div><i class="fa-solid fa-cube" style="color: #57b545;"></i> ` + data.online + `</div>`;
         widget = document.getElementById('minecraft');
         widget.innerHTML = innerhtml;
         setTimeout(minecraft_widget, 60000);
